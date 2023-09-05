ec46bfa3b1e4debe80c352fb9b85e81c06a9fac2
Dominic Purser <143379901+roybullard36@users.noreply.github.com>
2023-09-05 11:08:14 -0700

    Create w7x64.sh

---
+++ w7x64.sh
@@ -0,0 +1,29 @@
+apt-get update
+echo "Download windows files"
+wget -O w7x64.img https://bit.ly/akuhnetw7X64
+echo "Download ngrok"
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+read -p "Ctrl + V Authtoken: " CRP 
+./ngrok authtoken $CRP 
+nohup ./ngrok tcp 3388 &>/dev/null &
+echo Downloading File From akuh.net
+apt install qemu-kvm -y
+echo "Wait"
+echo "Starting Windows"
+qemu-system-x86_64 -hda win2012.img -m 4G -cpu host -smp cores=2,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device rtl8139,netdev=n0 -netdev user,id=n0 -vga qxl -accel kvm &>/dev/null &
+clear
+echo RDP Address:
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "===================================="
+echo "Username: akuh"
+echo "Password: Akuh.Net"
+echo "===================================="
+echo "===================================="
+echo "Keep supporting akuh.net, thank you"
+echo "You Got Free RDP now"
+echo "Wait 2 minute to finish bot"
+echo "You can close this tab"
+echo "RDP runs for 50 hours"
+echo "===================================="
+sleep 432000
